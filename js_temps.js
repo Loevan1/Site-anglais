c001339655c814497a011d2d043675b1ba8d7012
Loevan1 <loevangallais@gmail.com>
2025-10-26 19:03:59 +0100

    js et boutons

---
+++ js_temps.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#containere");
+const b_temps = document.querySelector('#b_temps');
+
+container.addEventListener('click',()=>{
+    
+    b_temps.style.display = "none";
+})
